b1655746563c078415926bfc730bab646375a5c5
WildNate <nathan.apple131@gmail.com>
2024-12-02 19:29:28 -0700

    added start stuff

--- main.py
+++ main.py
@@ -1,16 +1,14 @@
 import discord
 import os
-
 from dotenv import load_dotenv,dotenv_values
 from discord.ext import commands
 
 load_dotenv(".env")
+
 TOKEN: str = os.getenv("TOKEN")
 bot = commands.Bot(command_prefix = "!",intents=discord.Intents.all())
 
 @bot.event
 async def on_ready():
-    print("on")
-
-
+    print("Its Alive!!")
 bot.run(TOKEN)
